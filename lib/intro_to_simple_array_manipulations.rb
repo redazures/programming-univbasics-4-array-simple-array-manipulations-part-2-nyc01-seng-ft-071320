46a70628a0fdaf873cb0ad9214828f7e65027dfa
james wu <jwu_nyc@yahoo.com>
2020-06-26 21:55:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,7 +25,7 @@ end
 
 a=["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
 a.each do |item|
-  if a[item]==("Steven")
+  if item==("Steven")
     a.delete(item)
   else
   end  #a[item].delete if a[item]=s
