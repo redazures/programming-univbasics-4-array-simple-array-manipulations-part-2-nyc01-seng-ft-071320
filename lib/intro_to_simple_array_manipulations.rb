0120229f8b2479ef50777ec753376a83f343ea7a
james wu <jwu_nyc@yahoo.com>
2020-06-26 21:31:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,13 +6,18 @@ def using_insert(a,b)
   a.insert(3,b)
 end
 
-def using_uniq
+def using_uniq(a)
+  a.uniq
 end
 
-def using_flatten
+def using_flatten(a)
+  a.flatten
 end
 
-def using_delete
+def using_delete(a,s)
+  a.each do |item|
+    item.delete if item=s
+  end
 end
 
 def using_delete_at
